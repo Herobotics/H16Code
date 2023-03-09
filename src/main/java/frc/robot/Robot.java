3aaeab9d606859104bb1be1e7d32b708342016e1
Zero2Hero <herobotics2500@gmail.com>
2023-03-09 16:37:13 -0600

    added arm controls

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,6 +26,8 @@ public class Robot extends TimedRobot {
   MotorController m_rearLeft = new VictorSP(1);
   MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
 
+
+
   MotorController m_frontRight = new VictorSP(2);
   MotorController m_rearRight = new VictorSP(3);
   MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
@@ -33,10 +35,16 @@ public class Robot extends TimedRobot {
   Solenoid shiftSolenoidR = new Solenoid(PneumaticsModuleType.CTREPCM,  0);
   Solenoid shiftSolenoidL = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
 
+
+  Solenoid clawSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 2);
+  Solenoid armSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 3);
+  
   DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
   private final Joystick m_stick = new Joystick(0);
   public double driveScale = 0.75;
- 
+ public static double armScale = 0.2;
+MotorController armMotor = new VictorSP(4);
+
   public Encoder encoderR = new Encoder(0,1,false,Encoder.EncodingType.k2X);
   public Encoder encoderL = new Encoder(2,3, true, Encoder.EncodingType.k2X);
   
@@ -80,14 +88,32 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    if(m_stick.getRawButtonPressed(GamePad.Button.B)){
+    if(m_stick.getRawButtonPressed(GamePad.Button.BACK)){
       driveScale = driveScale * -1;
+    }
+    if(m_stick.getRawButtonPressed(GamePad.Button.B)){
+      clawSolenoid.toggle();
+    }
+    if(m_stick.getRawButtonPressed(GamePad.Button.A)){
+      armSolenoid.toggle();
+    }
+    if(m_stick.getRawButtonPressed(GamePad.Button.RIGHT_PRESS)){
+      armMotor.set(armScale);
+    }
+    if(m_stick.getRawButtonPressed(GamePad.Button.LEFT_PRESS)){
+      armMotor.set(armScale*-1.0);
+    }
+    if(m_stick.getRawButtonPressed(GamePad.Button.LEFT_PRESS)&&m_stick.getRawButtonPressed(GamePad.Button.RIGHT_PRESS)){
+      armMotor.set(0.0);
+    }
+    if(!m_stick.getRawButtonPressed(GamePad.Button.LEFT_PRESS)&&!m_stick.getRawButtonPressed(GamePad.Button.RIGHT_PRESS)){
+      armMotor.set(0.0);
     }
     double stickYval = -m_stick.getY() * driveScale;
     double stickXval = m_stick.getX() * driveScale;
     m_drive.arcadeDrive(stickYval, stickXval);
     //System.out.format("This is get Y: %d This is get X:%d", -m_stick.getY(), m_stick.getX());
- if(m_stick.getRawButtonPressed(GamePad.Button.A)) {
+ if(m_stick.getRawButtonPressed(GamePad.Button.X)) {
   shiftSolenoidL.toggle();
   shiftSolenoidR.toggle(); 
     }
