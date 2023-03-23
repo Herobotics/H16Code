23e87c94873e7ff4fc898e413f61361adbf263b1
Zero2Hero <herobotics2500@gmail.com>
2023-03-23 15:40:15 -0600

    we also added another autonomous mode

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,8 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// if reading this in the future dont use any of this unless you want a failure of a robot 
+// anyways this was written in march of 2023 so hello future
 
 package frc.robot;
 
